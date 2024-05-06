9ddad725d9efb322d124a913afbdd8f6106326fe
SSee <samantha.see@vtsd.com>
2024-05-06 11:44:39 -0400

    ver cuatro

--- main.py
+++ main.py
@@ -15,11 +15,23 @@ while add == "yes":
 else:
     study = input("Would like to study your terms?: ")
     if study == "yes":
-        print("Terms: " ,str(terms) , "Definitions(In order of their corresponding terms): " ,str(defins))
-        done_study = input("Would you like to add more terms?: ")
-        while done_study == "yes":
-            add == "yes"
-
+        print("Terms: ",str(terms), "Definitions(In order of their corresponding terms): ",str(defins))
+        add = input("Would you like to add another term?: ")
+        while add == "yes":
+            term4 = input("What term would you like to add?: ")
+            defin4 = input("Now define your term: ")
+            terms.append(term4)
+            defins.append(defin4)
+            add = input("Would you like to add another term?: ")
+        else:
+            study = input("Would like to study your terms?: ")
     else:
-        input("Would you like to add a term?: ")
-        add == "yes"
+        add = input("Would you like to add another term?: ")
+        if add == "yes":
+            term3 = input("What term would you like to add?: ")
+            defin3 = input("Now define your term: ")
+            terms.append(term3)
+            defins.append(defin3)
+            add2 = input("Would you like to add another term?: ")
+        else:
+            study = input("Would like to study your terms?: ")
